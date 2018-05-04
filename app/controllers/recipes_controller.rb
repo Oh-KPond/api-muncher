63dd0ca671f1509bce2f3025144e6563091e76d0
Kate Pond <kate@thepondsedge.com>
2018-05-03 17:32:51 -0700

    gets a basic view of the index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,21 @@
 class RecipesController < ApplicationController
+  # shows the results from query : recipes GET  /recipes(.:format)
   def index
+    if params[:query].blank?
+      redirect_to root_path
+      # flash
+    else
+      @recipes = EdamamApiWrapper.list_recipes(params[:query])
+      @query = params[:query]
+    end
   end
 
+  # shows a single recipe : recipe GET  /recipes/:id(.:format)
   def show
   end
 
+  # form for search also root
   def new
     @query = params[:query]
-  end
-
-  def create
-    redirect_to recipes_path
   end
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,8 @@
 class Recipe
-  attr_reader :title
+  attr_reader :title, :id
 
-  def initialize(title)
+  def initialize(title, id)
     @title = title
+    @id = id
   end
 end

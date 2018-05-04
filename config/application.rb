46a7e917fb173b794a8b23c30195bd1a35d83894
Kate Pond <kate@thepondsedge.com>
2018-05-04 14:14:40 -0700

    adds config.eager_load_paths to config for heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module ApiMuncher
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end

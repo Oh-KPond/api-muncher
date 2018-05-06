c7fc26756b2511870e9b51ab4a59709ca57b9400
Kate Pond <kate@thepondsedge.com>
2018-05-05 23:01:07 -0700

    adds tests for initializing recipe options

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -15,10 +15,25 @@ describe Recipe do
     }.must_raise ArgumentError
   end
 
-  it "Must initialize name & id properly" do
+  it "Must initialize name, id, & image properly" do
     recipe = Recipe.new("Title", "ID", "Image")
     recipe.title.must_equal "Title"
     recipe.id.must_equal "ID"
     recipe.img.must_equal "Image"
+  end
+
+  it "Must initialize options properly" do
+    options = {
+      source: "Source",
+      labels: "Lables",
+      ingredients: "Ingred",
+      url: "WWW"
+    }
+
+    recipe = Recipe.new("Title", "ID", "Image", options)
+    recipe.source.must_equal "Source"
+    recipe.labels.must_equal "Lables"
+    recipe.ingredients.must_equal "Ingred"
+    recipe.url.must_equal "WWW"
   end
 end

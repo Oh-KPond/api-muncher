f34f204493eddeb16330712be38e60e3940ca28f
Kate Pond <kate@thepondsedge.com>
2018-05-05 22:45:46 -0700

    creates more tests for edamam_api_wrapper

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -1,24 +1,47 @@
 require 'test_helper'
 
 describe EdamamApiWrapper do
-  it "can send a valid query to api" do
-    query = "soup"
+  describe "self.list_recipes" do
+    it "can send a valid query to api" do
+      query = "soup"
 
-    VCR.use_cassette("results") do
-      response = EdamamApiWrapper.list_recipes(query)
+      VCR.use_cassette("results") do
+        response = EdamamApiWrapper.list_recipes(query)
 
-      response.must_be_kind_of Array
-      response.first.must_be_kind_of Recipe
+        response.must_be_kind_of Array
+        response.first.must_be_kind_of Recipe
+      end
+    end
+
+    it "returns empty array for an invalid query" do
+      query = "$%*%"
+
+      VCR.use_cassette("results") do
+        response = EdamamApiWrapper.list_recipes(query)
+
+        response.must_be_empty
+      end
     end
   end
 
-  it "can retreve information about a recipe" do
-    id = "7543ecfa28b7506a97360748f017a83e"
+  describe "show_recipe" do
+    it "can retreve information about a recipe" do
+      id = "7543ecfa28b7506a97360748f017a83e"
 
-    VCR.use_cassette("results") do
-      response = EdamamApiWrapper.show_recipe(id)
-      
-      response.must_be_kind_of Recipe
+      VCR.use_cassette("results") do
+        response = EdamamApiWrapper.show_recipe(id)
+
+        response.must_be_kind_of Recipe
+      end
+    end
+
+    it "returns nil for invalid id" do
+      VCR.use_cassette("results") do
+        id = "000"
+        response = EdamamApiWrapper.show_recipe(id)
+
+        response.must_be_nil
+      end
     end
   end
 end
